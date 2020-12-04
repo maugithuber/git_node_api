f3486ff218621973b0f5a0a4f00669a9dcf63fa7
Carlos Mauricio Peñaranda Lino <carl_maurice@hotmail.com>
2020-12-04 08:56:33 -0400

    Actualizando el host de la base de datos a Atlas

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var port = 3800;
 
 mongoose.Promise = global.Promise; //Conectar a la bd
 // mongoose.connect('mongodb://localhost:27017/curso_mean_social')
-mongoose.connect('mongodb://admin:214049973@ds147518.mlab.com:47518/curso_mean_social') 
+//mongoose.connect('mongodb://admin:214049973@ds147518.mlab.com:47518/curso_mean_social') 
+mongoose.connect('mongodb+srv://admin:214049973@curso-mean-social.bhffb.mongodb.net/curso_mean_social?retryWrites=true&w=majority') 
+
 .then( () => {
         console.log("La conexion a la bd curso_mean_social se ha realizado correctamente!!!")
 
